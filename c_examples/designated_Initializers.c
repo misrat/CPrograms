add7bfa24cc82236941f36382e8798f8afe2263e
Olympus <olympusx85@gmail.com>
2016-07-24 10:10:53 -0700

    Examples01-2 using Designated Initializers

--- c_examples/designated_Initializers.c
+++ c_examples/designated_Initializers.c
@@ -10,14 +10,23 @@
 #include <stdlib.h>
 #include <string.h>
 
-// global constant
 // without designated initializers, "equal" will always be assigned to pos 0, master to pos 1 and slave to pos 2
 // with designated initializers - we can choose the array index and assign values to it. 
 // This is useful when array indexes have some meaning, and we can assign a string constant to the number(index)
-const char *arrayOfPointToChars_DI[] = {
+/*const char *arrayOfPointToChars_DI[] = {
     [1]    = "equal",
     [2]    = "master",
     [0]     = "slave",
+};*/
+
+#define A 1
+#define B 2
+#define C 3
+
+const char *arrayOfPointToChars_DI[] = {/* array of pointers to strings */
+    [A]    = "alphabet A ",
+    [B]    = "alphabet B ",
+    [C]    = "alphabet C"
 };
 
 //without designated initializers
@@ -26,17 +35,6 @@ const char *stringArray[] = {
     "master",
     "slave"
 };
-
-//void arrayInitialization(int n){
-    /* array of pointers to strings */
-//    const char *stringArray[] = {
-//        [1]    = "equal",
-//        [2]    = "master",
-//        [0]     = "slave",
-//    };
-//    printf("%s\n",stringArray[n] );
-//}
-
 
 int main(){
     printf("%s \n", stringArray[2]);
